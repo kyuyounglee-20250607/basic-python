8dfbf883419b43789f5a3b830ee86aa8365a68e8
lee <lkm1401@gmail.com>
2025-06-22 10:53:37 +0900

    .

--- exec.py
+++
@@ -1,6 +0,0 @@
-from test import TestClass
-
-def translate():
-    t = TestClass("test")
-    result = t.MyMethod("test", "en", "ko")
-    print(result)
--- test.py
+++
@@ -1,6 +0,0 @@
-class TestClass:
-    def __init__(self, value):
-        self.value = value
-
-    def MyMethod(self,text,src,desc):
-        return "mymethod"    
