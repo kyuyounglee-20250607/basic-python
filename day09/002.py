5eac29702bf0bd6937d4bcc16b6e50705a9db2c5
lee <lkm1401@gmail.com>
2025-07-05 11:13:43 +0900

    9일차.

--- day09/002.py
+++ day09/002.py
@@ -14,5 +14,9 @@ print(type(data[0]))
 print(f'강남역 7-10 : {data[0,7:11]}')
 # 강남역 하차인원 50명 이상인 시간대의 데이터
 print(data[0][ data[0] > 50 ])
+
+# 강남역 하차인원 50명 이상인 시간대
 indexs = np.where(data[0] > 50)
 print(f'index : {indexs}')
+# 해당 시간대의 하차인원
+print(f'index data : {data[0][indexs]}')
---
+++ day09/003.py
@@ -0,0 +1,3 @@
+import pandas as pd
+import numpy as np
+# csv 데이터 가져오기
