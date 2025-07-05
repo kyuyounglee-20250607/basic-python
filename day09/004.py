966a16ed5a8ecd03e089a23f564fa5952846dfad
lee <lkm1401@gmail.com>
2025-07-05 13:47:22 +0900

    9일차.

--- day09/004.py
+++ day09/004.py
@@ -19,3 +19,14 @@ print(samsung_prices.min())
 # 주가의 일간 변화량(오늘가격 - 어제가격)
 daily_returns = (samsung_prices[:-1] - samsung_prices[1:])/samsung_prices[1:]*100
 print(daily_returns)
+# 이동 평균
+data = np.array([1,2,3,4,5])
+windows = 3
+weight = np.ones(windows) / windows
+ma = np.convolve(data,weight,mode='valid')
+print(ma)
+# 주식데이터에서 이동평균(5일간)
+def move_averate(data, window):
+    return np.convolve(data,np.ones(window)/window,mode='valid')
+ma_5 = move_averate(samsung_prices,5)
+print(ma_5)
