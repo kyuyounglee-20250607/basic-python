c40f96b132fdda372548d874cd4e99d5c8d3594e
lee <lkm1401@gmail.com>
2025-07-05 11:27:53 +0900

    9일차.

--- day09/003.py
+++ day09/003.py
@@ -1,3 +1,14 @@
 import pandas as pd
 import numpy as np
 # csv 데이터 가져오기
+url = 'https://raw.githubusercontent.com/kyuyounglee-20250607/basic-python/refs/heads/main/day09/CARD_SUBWAY_MONTH_202505.csv'
+subway = pd.read_csv(url)
+print(subway)
+# 노선명만 출력해보기
+print(set(subway['노선명'].values))
+# 노선명중에 '강남'이포함된 노선명만 출력
+# '강남'이 포함된 노선명만 추출
+kangnam = {notion for notion in set(subway['노선명'].values) if '강남' in notion}
+print("\n'강남'이 포함된 노선명:")
+print(kangnam)
+# 강남노션에 대한 역명들을 출력
