9c1ebc05d657f94feaeb76604ada1909a03bc1a3
lee <lkm1401@gmail.com>
2025-07-05 12:13:50 +0900

    9일차

--- day09/003.py
+++ day09/003.py
@@ -2,20 +2,18 @@ import pandas as pd
 import numpy as np
 # csv 데이터 가져오기
 url = 'https://raw.githubusercontent.com/kyuyounglee-20250607/basic-python/refs/heads/main/day09/CARD_SUBWAY_MONTH_202505.csv'
-subway = pd.read_csv(url,header=1)
-subway = subway.dropna(axis=1, how='all')
-print(subway)
-
-# subway.columns = subway.columns.to_list() + ['NA']
-# 
+cols = pd.read_csv(url).columns.to_list()
+cols.append('na')
+subway = pd.read_csv(url,names=cols,header=0)
+subway = subway.dropna(axis=1)
 # print(subway)
-# # 노선명만 출력해보기
-# print(set(subway['노선명'].values))
-# # 노선명중에 '강남'이포함된 노선명만 출력
-# # '강남'이 포함된 노선명만 추출
-# kangnam = {notion for notion in set(subway['노선명'].values) if '강남' in notion}
-# print("\n'강남'이 포함된 노선명:")
-# print(kangnam)
+# 노선명만 출력해보기
+print(set(subway['노선명'].values))
+# 노선명중에 '강남'이포함된 노선명만 출력
+# '2호선'이 포함된 노선명만 추출
+line2 = {notion for notion in set(subway['노선명'].values) if '2호선' in notion}
+print("\n'2호선'이 포함된 노선명:")
+print(line2)
 # # 강남노션에 대한 역명들을 출력
 # # '강남'이 포함된 노선명에 해당하는 역명만 출력
 # filtered_stations = subway[subway['노선명'].str.contains('강남')]['역명']
